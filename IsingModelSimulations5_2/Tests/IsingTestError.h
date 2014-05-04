45494a0217be8adc92584cc382398a1951ad310b
kmkolasinski@gmail.com <kmkolasinski@gmail.com@a87264d8-99b0-a8f1-9d28-7b9d3e5ab4fc>
2014-05-04 16:45:22 +0000

    Travelers: small changes and new test class 1) added new test class for ERROR() function - IsingTestError. 2) added gnuplot script for IsingTestError class to plot results 3) uploaded other results obtained from another tests

---
+++ IsingModelSimulations5_2/Tests/IsingTestError.h
@@ -0,0 +1,70 @@
+/* 
+* File:   IsingTestError.h
+* Author: alina
+*
+* Created on 4 maj 2014, 18:11
+*/
+
+#ifndef ISINGTESTERROR_H
+#define	ISINGTESTERROR_H
+
+#include "IsingTest.h"
+
+class IsingTestError : public IsingTest {
+
+   public:
+       IsingTestError():IsingTest(){
+           test_name = "Test of error (bootstrap method).";   
+           test_info = string(" Run test to calculate the erro value in function of \n")+
+                       string(" the production time.\n The results are saved in proper files:")+  
+                       string(" 'IsingTestError.txt' and 'IsingTestError.png' in 'tests_out' directory. \n")+
+                       string(" See run() function for more details.");
+           
+           info(); 
+           run(); // run all calculations
+           string cmd =string("cd ")+test_dir_output+string(";gnuplot IsingTestError.plt");
+           int info = system(cmd.c_str());
+       }   
+       
+       /**
+        * Tests Chi in function of Temperature for 1D model
+        */
+       void run(){
+        
+       cout << " Running test..." << endl;    
+           
+       //SIMULATION INPUT DATA
+       int chainLength      = 500;        
+       double magneticField = 0.0;
+       int therm_t          = 500;   //thermalization time
+       int measure_f        = 1;     //measure frequency
+
+       int maxGdist         = 10;     //domain of correlation function extends from 0 to maxGdist-1
+       int maxTsep          = 2;
+       double initState     = 0.7;    //values between 0.5 and 1.0
+       string fileout = test_dir_output+"IsingTestError.txt";
+       ofstream data_out(fileout.c_str());
+       
+       cout << " Start prod_t=" << 1000 << endl;
+       cout << " End   prod_t=" << 100000 << endl;
+       cout << " Mult  prod_t=" << 2 << endl;
+       cout << " It may take some time..." << endl;
+       cout << " prod_t   " << " Error " << endl;
+       cout << "----------------------------------------------------" << endl;
+       
+       double temperature = 1.0;
+       Ising chain(chainLength, temperature, magneticField, initState, maxGdist, maxTsep);
+       for ( int prod_t = 1000 ; prod_t < 100000 ; prod_t *= 2  ){
+           chain.MC_simulation(therm_t, prod_t, measure_f);
+           double error=chain.ERROR(gettotalFname().c_str(), ERROR_CHI);
+           data_out << std::scientific <<  prod_t << "\t" << error  << "\n";
+           cout     << std::scientific <<  prod_t << "\t" << error  << "\n";
+       }// end of for(prod_t)
+       
+       data_out.close();                                    
+       }// end of run()
+
+};
+
+
+#endif	/* ISINGTESTERROR_H */

1051edeaa298868ae794ad3d65f716765726ce3d
t.wydro@gmail.com <t.wydro@gmail.com@a87264d8-99b0-a8f1-9d28-7b9d3e5ab4fc>
2014-05-20 13:19:33 +0000

    Another version of cluster distribution algorithm

--- IsingModelSimulations5_2/IsingMain5_2.cpp
+++ IsingModelSimulations5_2/IsingMain5_2.cpp
@@ -39,23 +39,39 @@
 using namespace std;
 
 int main(int argc, char** argv) {
+
+//CALCULATION OF PHYSICAL PROPERTIES OF ISING MODEL IN ONE AND TWO DIMENSIONS 
+//                 IN FUNCTION OF THE TEMPERATURE. 
+//       Periodic boundary conditions are assumed in all cases.    
+//Special attention is paid to behavior close to criticality i.e. to T=0 in 1D
+//and to T=2.27 for 2D square lattice.
+//To run any of the following test please uncomment it, compile and run the program.
+//To modify input data edit test and change the value of parameters, then compile and run.    
     
     // ----------------------------------------------------------
     // Comment or uncomment following tests:
     // ----------------------------------------------------------
-
+   
+//1D ISING CHAIN WITH PERIODIC BOUNDARY CONDITIONS 
     // IsingTestChi isingTestChi;     //  Ising1D test of Chi in function of T
     // IsingTestError isingTestError; //  Ising1D test of ERROR funtion for Chi for different prod_
     // IsingTestClusterStat isingTestClusterStat;  //Ising1D test of cluster distribution
+    // IsingTestCorrelationLength isingTestCorrelationLength;
+    // IsingTestMeanECorrelation isingTestMeanECorrelation;
     // IsingTestTCorr isingTestTCorr; // 
+    // IsingTestMeanClusterFreq1D isingTestMeanClusterFreq1D; // Ising1D test for mean cluster size distribution
+    // IsingTestRenormGroup1D  isingTestRenormGroup1D; // Ising1D test for renormalization group
+    
+//2D ISING MODEL ON SQUARE LATTICE WITH PERIODIC BOUNDARY CONDITIONS 
     // IsingTestCC2D isingTestCC2D;   //  Ising2D test of CC in function of T for different lattices
     // IsingTestM2D isingTestM2D;     //  Ising2D test of M in function of T for different lattices        
     // IsingTestExact2D isingTestExact2D; // Ising2D test which calculates the exacts values of Cv for  small lattices
-    // IsingTestRenormGroup1D  isingTestRenormGroup1D; // Ising1D test for renormalization group
-    // IsingTestMeanClusterFreq1D isingTestMeanClusterFreq1D; // Ising1D test for mean cluster size distribution
-    // IsingTestCorrelationLength isingTestCorrelationLength;
-    // IsingTestMeanECorrelation isingTestMeanECorrelation;
-    
+
+//TOOLS
+    // TestError TestError; //  Generic test of ERROR funtion for some input data 
+    //                           and any algorithm serving for calculation of physical
+    //                           quantity.
+        
     cout << "\n\n     T H E   E N D " << endl;
     return 0;
 }

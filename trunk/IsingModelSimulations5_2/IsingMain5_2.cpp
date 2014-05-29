b314a040c2b3a3a1a55b7abfccec47a40052cce9
edytaosika@gmail.com <edytaosika@gmail.com@a87264d8-99b0-a8f1-9d28-7b9d3e5ab4fc>
2014-05-29 02:14:07 +0000

    Travelers: changes in IsingTestRenormGroup1D, documentation for IsingTestRenormGroup1D added

--- trunk/IsingModelSimulations5_2/IsingMain5_2.cpp
+++ trunk/IsingModelSimulations5_2/IsingMain5_2.cpp
@@ -68,12 +68,12 @@ int main(int argc, char** argv) {
     // IsingTestMeanECorrelation isingTestMeanECorrelation;
     // IsingTestTCorr isingTestTCorr; // 
     // IsingTestMeanClusterFreq1D isingTestMeanClusterFreq1D; // Ising1D test for mean cluster size distribution
-    // IsingTestRenormGroup1D  isingTestRenormGroup1D; // Ising1D test for renormalization group
+     IsingTestRenormGroup1D  isingTestRenormGroup1D; // Ising1D test for renormalization group
     // IsingTestE1D isingTestE1D; //Ising1D test for energy behavior of the system.
     // IsingTestExact1D isingTestExact1D; // Ising1D calulation of extact values of Cv for 1D chain
     
 //2D ISING MODEL ON SQUARE LATTICE WITH PERIODIC BOUNDARY CONDITIONS 
-     IsingTestChi2D isingTestChi2D;  //  Ising2D test of Chi in function of T
+    // IsingTestChi2D isingTestChi2D;  //  Ising2D test of Chi in function of T
     // IsingTestError2D isingTestError2D;   //  Ising2D test of ERROR funtion for Chi for different prod_time
     // IsingTestCC2D isingTestCC2D;   //  Ising2D test of CC in function of T for different lattices
     // IsingTestM2D isingTestM2D;     //  Ising2D test of M in function of T for different lattices        
